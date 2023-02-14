3b7541ba87f4916ecc2bfc08806a78b918abe2ac
PauloVitGod12 <Paulo@vm-Paulo>
2023-02-14 17:08:34 +0000

    Dbcontext  packages entity framework Database constructor Connection string adhered

---
+++ dotnetflix/Data/Dbdotnetflix.cs
@@ -0,0 +1,13 @@
+﻿using dotnetflix.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace dotnetflix.Data
+{
+    public class Dbdotnetflix : DbContext
+    {
+        public Dbdotnetflix(DbContextOptions<Dbdotnetflix> options) : base(options) { }
+
+
+    }
+}
+
